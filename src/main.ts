8b2c669e9b77a52595f70ad53866248adab0b752
vladislav <vlad@itpw.ru>
2022-06-02 12:17:52 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,11 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+
+// echo "# storyroom_manager" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M master
+// git remote add origin https://github.com/CrazyMasters/storyroom_manager.git
+// git push -u origin master
